ccb1a7e5341b43eb7b5dc683c2701f55db294fea
liubenben <qieda@163.com>
2022-05-15 15:57:24 +0800

    update to v2.2.-

--- src/Components/Utils/TestBar.js
+++ src/Components/Utils/TestBar.js
@@ -1,22 +1,14 @@
-import { isAddress } from 'ethers/lib/utils';
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { ExclamationCircle } from 'react-bootstrap-icons';
-import { getNamesOfOwner } from '../Global/globals';
-//import moment from 'moment'
-// import { BigNumber, utils } from 'ethers';
-// import { hexlify } from 'ethers/lib/utils';
-// import confFile from '../../conf.json'
 
 
 export default function TestBar(props) {
 
   const testFunc1 = async () => {
-    getNamesOfOwner("0xd5D171a9AA125AF13216C3213B5A9Fc793FcCF2c")
   }
 
   const testFunc2 = async () => {
-    console.log(isAddress("0xd5D171a9AA125AF13216C3213B5A9Fc793FcCF2c"))
   }
 
   return (
