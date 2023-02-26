957f59e2670797cfc8402a14808fa745df04c417
liubenben <qieda@163.com>
2023-02-26 14:57:13 +0800

    upgrade to react 18 to eradicate the process bugs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
 import "./i18n";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById("root")
 );
