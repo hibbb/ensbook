287390218bcd57e54e899c0e040233218451abe0
lbbfor@gmail.com <lbbfor@gmail.com>
2025-12-14 16:25:02 +0800

    install tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    // 确保包含所有可能使用 Tailwind 类名的文件
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
