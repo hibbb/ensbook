cd4355fb60118942f5db9d3415b5a810272e926d
lbbfor@gmail.com <lbbfor@gmail.com>
2026-01-07 10:54:28 +0800

    chore(tooling): 初始化 husky 和 commitlint 以执行提交信息校验

---
+++ commitlint.config.cjs
@@ -0,0 +1,27 @@
+// commitlint.config.cjs
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-case": [2, "always", "lower-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 100],
+  },
+};
