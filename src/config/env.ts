81131cb27e415450abf0756f2c31a71009d857f4
lbbfor@gmail.com <lbbfor@gmail.com>
2026-01-16 14:58:05 +0800

    chore: 整理 VITE_ENS_REFERRER_HASH

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,9 +5,7 @@ import { type Hex, pad } from "viem";
  * 处理 Referrer 地址
  * 逻辑：读取环境变量 -> 如果没有则使用全零地址 -> 确保转换为 32 字节的 Hex 格式
  */
-const rawReferrer =
-  import.meta.env.VITE_ENS_REFERRER_HASH ||
-  "0x0000000000000000000000000000000000000000";
+const rawReferrer = "0x9c6aa5ce4903aad922ac4dde9b57817c1fc17d9b";
 
 // 预先计算并导出，Hook 中直接使用，无需重复计算
 export const REFERRER_ADDRESS_HASH: Hex = pad(
